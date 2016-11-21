4e2fa02d297e00c3912aa087e2aa079435832fb6
edus44 <eduhhg@gmail.com>
2016-11-21 18:20:10 +0100

    improved light management

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 'use strict';
 
+const moment = require('moment');
 const tv = require('./lib/tv')
-const rele = require('./lib/rele')
+const Rele = require('./lib/Rele')
 
+const amp = new Rele(18,'amp')
+const light = new Rele(24,'light')
 tv.start()
-rele.start()
 
 tv.on('power-on',()=>{
-    console.log('ON');
-    rele.val(1,0)
-    rele.val(2,0)
+    console.log('on');
+    amp.on()
+    light.restore()
 })
 tv.on('power-off',()=>{
-    console.log('OFF');
-    rele.val(1,1)
-    rele.val(2,1)
+    console.log('off');
+    amp.off()
+
+    light.save()
+    light.off()
 })
 tv.on('key',(code,name)=>{
     console.log('key',code,name);
-	rele.val(1, rele.val(1) ? 0 : 1 )
-    
+    light.toggle()
 })
+
+
+function isNight(){
+    return moment('19:00','HH:mm').isAfter()
+}
 
 process.on( 'SIGINT', function() {
     tv.stop();
-    rele.stop();
+    amp.stop();
+    light.stop();
     process.exit();
 });
