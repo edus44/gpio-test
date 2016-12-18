4dcf0a10c0be204903fd5786b306898cbaee45f5
edus44 <eduhhg@gmail.com>
2016-12-18 19:17:50 +0000

    automatic set light on or off depending on day hour

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 'use strict';
 
-const moment = require('moment');
 const tv = require('./lib/tv')
 const Rele = require('./lib/Rele')
 
 const amp = new Rele(18,'amp')
 const light = new Rele(24,'light')
+let manual = false;
+
 tv.start()
 
 tv.on('power-on',()=>{
-    console.log('on');
+    manual = false;
     amp.on()
-    light.restore()
+    light.set( isNight() )
 })
 tv.on('power-off',()=>{
-    console.log('off');
+    manual = false;
     amp.off()
 
-    light.save()
+    light.off()
     light.off()
 })
 tv.on('key',(code,name)=>{
-    console.log('key',code,name);
+    manual = true;
     light.toggle()
 })
 
 
 function isNight(){
-    return moment('19:00','HH:mm').isAfter()
+    var hour = (new Date()).getHours()|0
+    return hour > 17 || hour < 7
 }
+
+setInterval(function(){
+    if (tv.powerStatus===false) 
+        return;
+
+    if (isNight() && light.status===false && !manual){
+        console.log('Forced light on')
+        light.on()
+    }
+    if (!isNight() && light.status===true){
+        console.log('Forced light off')
+        light.off()
+    }
+},30000)
+
 
 process.on( 'SIGINT', function() {
     tv.stop();
