697ce0b796c590d193b4d0da759a1323cd1b0502
edus44 <eduhhg@gmail.com>
2018-07-27 00:48:17 +0200

    prettied and on endpoint

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-'use strict';
+'use strict'
 const Button = require('./lib/Button')
 const tv = require('./lib/tv')
 const Rele = require('./lib/Rele')
 const api = require('./lib/api')
 
-const amp = new Rele(17,'amp')
-const light = new Rele(18,'light')
-const ampSwitch = new Button(6,500)
-let manual = false;
+const amp = new Rele(17, 'amp')
+const light = new Rele(18, 'light')
+const ampSwitch = new Button(6, 500)
+let manual = false
 
 tv.start()
 
-tv.on('power-on',()=>{
-    manual = false;
-    amp.on()
-    light.set( isNight() )
+tv.on('power-on', () => {
+  manual = false
+  amp.on()
+  light.set(isNight())
 })
-tv.on('power-off',()=>{
-    manual = false;
-    amp.off()
+tv.on('power-off', () => {
+  manual = false
+  amp.off()
 
-    light.off()
+  light.off()
 })
-tv.on('key',(code,name)=>{
-    manual = true;
-    light.toggle()
-})
-
-ampSwitch.on('pushed',()=>{
-    amp.toggle()
+tv.on('key', (code, name) => {
+  manual = true
+  light.toggle()
 })
 
-function isNight(){
-    var hour = (new Date()).getHours()|0
-    return hour > 17 || hour < 7
+ampSwitch.on('pushed', () => {
+  amp.toggle()
+})
+
+function isNight() {
+  var hour = new Date().getHours() | 0
+  return hour > 17 || hour < 7
 }
 
-setInterval(function(){
-    if (tv.powerStatus===false) 
-        return;
+setInterval(function() {
+  if (tv.powerStatus === false) return
 
-    if (isNight() && light.status===false && !manual){
-        console.log('Forced light on')
-        light.on()
-    }
-    if (!isNight() && light.status===true){
-        console.log('Forced light off')
-        light.off()
-    }
-},30000)
+  if (isNight() && light.status === false && !manual) {
+    console.log('Forced light on')
+    light.on()
+  }
+  if (!isNight() && light.status === true) {
+    console.log('Forced light off')
+    light.off()
+  }
+}, 30000)
 
-
-process.on( 'SIGINT', function() {
-    tv.stop();
-    amp.stop();
-    light.stop();
-    process.exit();
-});
+process.on('SIGINT', function() {
+  tv.stop()
+  amp.stop()
+  light.stop()
+  process.exit()
+})
